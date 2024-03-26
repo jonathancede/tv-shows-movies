c48ee119d21afa7da0cc1870316f52d0cbef4cbe
Jonathan <jony.cede@gmail.com>
2024-03-26 14:36:44 +0100

    Added HttpService and TMDBService

---
+++ src/services/http_service.ts
@@ -0,0 +1,74 @@
+import axios, { AxiosInstance, AxiosResponse, AxiosError } from "axios"
+
+export type GeneralCatchFunctionType = (error: any) => void
+
+class HttpService {
+  private axiosInstance: AxiosInstance
+  private generalCatch?: GeneralCatchFunctionType
+
+  constructor(baseUrl?: string, apiKey?: string, generalCatch?: GeneralCatchFunctionType) {
+    this.axiosInstance = axios.create({
+      baseURL: baseUrl ?? "",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${apiKey ?? ""}`,
+      },
+    })
+    this.generalCatch = generalCatch
+  }
+
+  private handleGeneralCatch(error: AxiosError) {
+    if (this.generalCatch) this.generalCatch(error)
+  }
+
+  async get<T>(url: string): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.get<T>(url)
+      return response.data
+    } catch (error: any) {
+      this.handleGeneralCatch(error)
+      throw new Error(`Error al realizar la solicitud GET: ${error}`)
+    }
+  }
+
+  async post<T>(url: string, data: any): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.post<T>(url, data)
+      return response.data
+    } catch (error: any) {
+      this.handleGeneralCatch(error)
+      throw new Error(`Error al realizar la solicitud POST: ${error}`)
+    }
+  }
+
+  async put<T>(url: string, data: any): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.put<T>(url, data)
+      return response.data
+    } catch (error: any) {
+      this.handleGeneralCatch(error)
+      throw new Error(`Error al realizar la solicitud PUT: ${error}`)
+    }
+  }
+
+  async patch<T>(url: string, data: any): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.patch<T>(url, data)
+      return response.data
+    } catch (error: any) {
+      this.handleGeneralCatch(error)
+      throw new Error(`Error al realizar la solicitud PATCH: ${error}`)
+    }
+  }
+
+  async delete<T>(url: string): Promise<void> {
+    try {
+      await this.axiosInstance.delete(url)
+    } catch (error: any) {
+      this.handleGeneralCatch(error)
+      throw new Error(`Error al realizar la solicitud DELETE: ${error}`)
+    }
+  }
+}
+
+export default HttpService
---
+++ src/services/tmdb_api_service.ts
@@ -0,0 +1,15 @@
+import HttpService, { GeneralCatchFunctionType } from "./http_service"
+
+class TMDBApiService extends HttpService {
+  constructor(baseUrl?: string, apiKey?: string, generalCatch?: GeneralCatchFunctionType) {
+    super(baseUrl, apiKey, generalCatch)
+  }
+
+  async getPopularMovies(language: string, page: string | number): Promise<any> {
+    return await this.get(`movie/popular?language=${language}&page=${page}`)
+  }
+}
+
+const TMDB_URL = process.env.NEXT_PUBLIC_TMDB_URL
+const TMDB_API_AUTH_TOKEN = process.env.NEXT_PUBLIC_TMDB_API_AUTH_TOKEN
+export default new TMDBApiService(TMDB_URL, TMDB_API_AUTH_TOKEN)
